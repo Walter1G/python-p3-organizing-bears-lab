b8133bc425f8b471a39ca65a31a66058d6abca58
Walter N <2760walter@gmail.com>
2023-12-05 16:38:34 +0300

    COMPLETE

---
+++ lib/create.sql
@@ -0,0 +1,7 @@
+CREATE TABLE bears( id INTEGER,
+   name TEXT,
+   age INTEGER,
+   sex TEXT,
+   color TEXT,
+   temperament TEXT,
+   alive INTEGER);
---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES( 1, "Mr. Chocolate", 32, "M","red","temp",1);
+INSERT INTO bears VALUES( 2, "Rowdy", 32, "M","red","temp",1);
+INSERT INTO bears VALUES( 3, "Tabitha", 32, "F","green","temp",1);
+INSERT INTO bears VALUES( 4, "Sergeant Brown", 32, "M","red","temp",1);
+INSERT INTO bears VALUES( 5, "Melissa", 32, "F","violet","temp",0);
+INSERT INTO bears VALUES( 6, "Grinch", 32, "M","blue","temp",1);
+INSERT INTO bears VALUES( 7, "Wendy", 32, "F","pink","temp",1);
+INSERT INTO bears VALUES( 8, NULL, 32, "M","yellow","temp",0);
